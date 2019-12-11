1707c2f54aa68aaf08f4ee0f60473f58924f462c
anup03gupta <anup03gupta@gmail.com>
2019-12-11 14:33:36 +0530

    Add files via upload
    Cors issue is resolved.

--- SB-Payment/src/main/java/com/zensar/broker/serviceImpl/AccountServiceImpl.java
+++ SB-Payment/src/main/java/com/zensar/broker/serviceImpl/AccountServiceImpl.java
@@ -1,6 +1,12 @@
 package com.zensar.broker.serviceImpl;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.zensar.broker.dto.AccountDTO;
@@ -17,14 +23,24 @@ public class AccountServiceImpl implements AccountService {
 	
 	@Autowired
 	BeneficiaryRepo beneficiaryRepo;
-	
+
 	@Override
-	public Iterable<AccountDTO> getAccounts() {
-		return accountRepo.findAll();
+	public Iterable<AccountDTO> getAccounts(Integer pageNo, Integer pageSize, String sortBy, String order) {
+		Sort sort = ((order.equalsIgnoreCase("DESC") ? Sort.by(sortBy).descending() : Sort.by(sortBy).ascending()));
+		
+		Pageable paging = PageRequest.of(pageNo, pageSize, sort);
+		Page<AccountDTO> pagedResult = accountRepo.findAll(paging);
+		
+		return ((pagedResult.hasContent()) ? pagedResult.getContent() : new ArrayList<AccountDTO>());
 	}
 
 	@Override
-	public Iterable<BeneficiaryDTO> getBeneficiariesbyAccountId(Long id) {
-		return beneficiaryRepo.findByFromAccountId(id);
+	public Iterable<BeneficiaryDTO> getBeneficiariesbyAccountId(Long id, Integer pageNo, Integer pageSize, String sortBy, String order) {
+		Sort sort = ((order.equalsIgnoreCase("DESC") ? Sort.by(sortBy).descending() : Sort.by(sortBy).ascending()));
+		
+		Pageable paging = PageRequest.of(pageNo, pageSize, sort);
+		Page<BeneficiaryDTO> pagedResult = beneficiaryRepo.findByFromAccountId(id, paging);
+		
+		return ((pagedResult.hasContent()) ? pagedResult.getContent() : new ArrayList<BeneficiaryDTO>());
 	}
 }
--- SB-Payment/src/main/java/com/zensar/broker/serviceImpl/CreatePaymentServiceImpl.java
+++ SB-Payment/src/main/java/com/zensar/broker/serviceImpl/CreatePaymentServiceImpl.java
@@ -1,11 +1,16 @@
 package com.zensar.broker.serviceImpl;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.zensar.broker.bean.CreateNewPaymentRequest;
@@ -20,60 +25,65 @@ import com.zensar.broker.service.CreatePaymentService;
 
 @Service
 public class CreatePaymentServiceImpl implements CreatePaymentService {
-	
+
 	@Autowired
 	CreatePaymentDao createPaymentDao;
-	
+
 	@Autowired
 	PaymentRepo paymentRepo;
-	
+
 	@Autowired
 	AccountRepo accountRepo;
-	
+
 	@Autowired
 	BeneficiaryRepo beneficiaryRepo;
-	
+
 	@Override
 	public List<AccountDTO> getAccountDetails() {
 		return createPaymentDao.getAccountDetails();
 	}
 
 	@Override
-	public Iterable<PaymentDTO> getAllPayments() {
-		return paymentRepo.findAll();
+	public Iterable<PaymentDTO> getAllPayments(Integer pageNo, Integer pageSize, String sortBy, String order) {
+		Sort sort = ((order.equalsIgnoreCase("DESC") ? Sort.by(sortBy).descending() : Sort.by(sortBy).ascending()));
+
+		Pageable paging = PageRequest.of(pageNo, pageSize, sort);
+		Page<PaymentDTO> pagedResult = paymentRepo.findAll(paging);
+		
+		return ((pagedResult.hasContent()) ? pagedResult.getContent() : new ArrayList<PaymentDTO>());
 	}
-	
+
 	@Transactional
 	@Override
 	public String createNewPayment(CreateNewPaymentRequest payment) {
-		
+
 		PaymentDTO newPayment = new PaymentDTO();
 		newPayment.setAmount(payment.getAmount());
 		newPayment.setBatchDescription(payment.getVarialeReference());
 		newPayment.setBatchId(payment.getBeneficiaryId());
 		newPayment.setStatus("Success");
 		newPayment.setValueDate(LocalDate.now());
-		
+
 		AccountDTO fromAccount = accountRepo.findById(payment.getFromAccountId()).get();
 		if (fromAccount.getAmountAvaiable() < payment.getAmount()) {
 			return "Insufficient balance.";
 		}
-		
+
 		newPayment.setFromAccount(fromAccount.getAccountName());
 		newPayment.setFromAccountId(fromAccount);
-		
+
 		BeneficiaryDTO beneficiaryDTO = beneficiaryRepo.findById(payment.getBeneficiaryId()).get();
 		AccountDTO toAccount = beneficiaryDTO.getToAccount();
 		newPayment.setToAccountId(toAccount);
-		
+
 		paymentRepo.save(newPayment);
-		
-		fromAccount.setAmountAvaiable( (fromAccount.getAmountAvaiable() -  payment.getAmount()) );
+
+		fromAccount.setAmountAvaiable((fromAccount.getAmountAvaiable() - payment.getAmount()));
 		accountRepo.save(fromAccount);
-		
-		toAccount.setAmountAvaiable( (toAccount.getAmountAvaiable() + payment.getAmount()) );
+
+		toAccount.setAmountAvaiable((toAccount.getAmountAvaiable() + payment.getAmount()));
 		accountRepo.save(toAccount);
-		
+
 		return "Transaction successful.";
 	}
 
